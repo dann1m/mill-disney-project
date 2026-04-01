ca67128b4e9d69af7cff35f524446bc739090468
Charlotte Li <ccl247@cornell.edu>
2026-04-01 03:33:47 +0000

    created stock csv file

--- stock.py
+++ stock.py
@@ -4,4 +4,7 @@ import pandas as pd
 # Download Disney stock data (2015–2020)
 dis = yf.download("DIS", start="2015-01-01", end="2020-12-31")
 
-print(dis.head())
+# Calculate daily returns using Adjusted Close
+dis['Daily Return'] = dis['Close'].pct_change()
+
+dis.to_csv("disney_stock_2015_2020.csv ", index=True)
